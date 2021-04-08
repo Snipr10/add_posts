60e51681c39c44aa6312e82ac5d0ebb545cef6ac
oleg <olegsh-gusev@mail.ru>
2021-04-08 10:59:22 +0300

    test

--- core/views.py
+++ core/views.py
@@ -40,7 +40,7 @@ class Task(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kwargs):
         try:
-            return Response("ok"  + str(len(models.Task.objects.filter(id=1))))
+            return Response("ok"  + str(len(models.Task.objects.filter(id=6008))))
         except Exception as e:
             return Response(e)
 
