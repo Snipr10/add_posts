af87f8868f4f7dd2fb770015c4b58ac05af07e0d
oleg <olegsh-gusev@mail.ru>
2021-04-08 12:44:22 +0300

    test

--- add_posts/tasks.py
+++ add_posts/tasks.py
@@ -79,7 +79,7 @@ def start_parsing_url():
             content = models.Content.objects.create(text=text)
             print("content" + str(content.id))
             print("post_url.task_id" + str(post_url.task_id))
-            task = models.Task.objects.get(int(post_url.task_id))
+            task = models.Task.objects.get(id = int(post_url.task_id))
             print("task" + str(task.id))
 
             post = models.Post.objects.create(content=content, task=task,
