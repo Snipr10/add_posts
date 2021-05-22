8711975a4afeb4816384f4545c0a1b5eee892c57
oleg <olegsh-gusev@mail.ru>
2021-05-22 23:20:32 +0300

    test

--- add_posts/celery/celery.py
+++ add_posts/celery/celery.py
@@ -13,25 +13,25 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'start_parsing_url': {
-        'task': 'add_posts.tasks.start_parsing_url',
-        'schedule': crontab(
-            minute='*/3')
-    },
-    'update_proxy': {
-        'task': 'add_posts.tasks.update_proxy',
-        'schedule': crontab(
-            minute='*/2')
-    },
-    'delete_bad_worker_credentials': {
-        'task': 'add_posts.tasks.delete_bad_worker_credentials',
-        'schedule': crontab(
-            minute='*/1')
-    },
+    # 'start_parsing_url': {
+    #     'task': 'add_posts.tasks.start_parsing_url',
+    #     'schedule': crontab(
+    #         minute='*/3')
+    # },
+    # 'update_proxy': {
+    #     'task': 'add_posts.tasks.update_proxy',
+    #     'schedule': crontab(
+    #         minute='*/2')
+    # },
+    # 'delete_bad_worker_credentials': {
+    #     'task': 'add_posts.tasks.delete_bad_worker_credentials',
+    #     'schedule': crontab(
+    #         minute='*/1')
+    # },
     'check_not_available_accounts': {
         'task': 'add_posts.tasks.check_not_available_accounts',
         'schedule': crontab(
-            minute='*/35')
+            minute='*/2')
     }
 
 
--- add_posts/tasks.py
+++ add_posts/tasks.py
@@ -172,6 +172,7 @@ def check_proxy(url, attempt=0):
 
 def get_available_proxy():
     proxies = models.WorkCredentials.objects.all().values_list('proxy', flat=True)
+    print(proxies)
     proxy = models.Proxy.objects.filter(available=True, expirationDate__gte=datetime.now())\
         .exclude(id__in=proxies).order_by(
         "id").last()
