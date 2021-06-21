5314e43fda3fa67fec08d6929767228d8908a651
oleg <olegsh-gusev@mail.ru>
2021-06-21 16:16:30 +0300

    fix

--- add_posts/celery/celery.py
+++ add_posts/celery/celery.py
@@ -13,26 +13,26 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'start_parsing_url': {
-        'task': 'add_posts.tasks.start_parsing_url',
-        'schedule': crontab(
-            minute='*/3')
-    },
+    # 'start_parsing_url': {
+    #     'task': 'add_posts.tasks.start_parsing_url',
+    #     'schedule': crontab(
+    #         minute='*/3')
+    # },
     'update_proxy': {
         'task': 'add_posts.tasks.update_proxy',
         'schedule': crontab(
             minute='*/2')
     },
-    'delete_bad_worker_credentials': {
-        'task': 'add_posts.tasks.delete_bad_worker_credentials',
-        'schedule': crontab(
-            minute='*/1')
-    },
-    'check_not_available_accounts': {
-        'task': 'add_posts.tasks.check_not_available_accounts',
-        'schedule': crontab(
-            minute='*/3')
-    },
+    # 'delete_bad_worker_credentials': {
+    #     'task': 'add_posts.tasks.delete_bad_worker_credentials',
+    #     'schedule': crontab(
+    #         minute='*/1')
+    # },
+    # 'check_not_available_accounts': {
+    #     'task': 'add_posts.tasks.check_not_available_accounts',
+    #     'schedule': crontab(
+    #         minute='*/3')
+    # },
     # 'delete_old_proxy': {
     #     'task': 'add_posts.tasks.delete_old_proxy',
     #     'schedule': crontab(
