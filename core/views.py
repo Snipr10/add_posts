ee69affa9a5d897aa9e5bd4fbfa622973818d972
oleg <gusevoleg@gmail.com>
2021-10-09 21:15:09 +0300

    add reset_task_by_key

--- core/views.py
+++ core/views.py
@@ -123,6 +123,7 @@ def reset_task_by_key(request):
             task_keyword.save(update_fields=['status'])
             return Response("ok", status=status.HTTP_200_OK)
     except Exception as e:
+        print(e)
         return Response(str(e),
                         status=status.HTTP_400_BAD_REQUEST)
 
