09cd6b49c7b2a17910ea4874b52fa087453d81c0
oleg <gusevoleg@gmail.com>
2022-02-26 09:05:01 +0300

    fix

--- add_posts/celery/celery.py
+++ add_posts/celery/celery.py
@@ -13,26 +13,26 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    'start_update_content': {
-        'task': 'add_posts.tasks.start_update_content',
-        'schedule': crontab(
-            hour='*/1')
-    },
-    'update_proxy': {
-        'task': 'add_posts.tasks.update_proxy',
-        'schedule': crontab(
-            minute='*/10')
-    },
-    'delete_bad_worker_credentials': {
-        'task': 'add_posts.tasks.delete_bad_worker_credentials',
-        'schedule': crontab(
-            minute='*/10')
-    },
-    'check_not_available_accounts': {
-        'task': 'add_posts.tasks.check_not_available_accounts',
-        'schedule': crontab(
-            minute='*/50')
-    },
+    # 'start_update_content': {
+    #     'task': 'add_posts.tasks.start_update_content',
+    #     'schedule': crontab(
+    #         hour='*/1')
+    # },
+    # 'update_proxy': {
+    #     'task': 'add_posts.tasks.update_proxy',
+    #     'schedule': crontab(
+    #         minute='*/10')
+    # },
+    # 'delete_bad_worker_credentials': {
+    #     'task': 'add_posts.tasks.delete_bad_worker_credentials',
+    #     'schedule': crontab(
+    #         minute='*/10')
+    # },
+    # 'check_not_available_accounts': {
+    #     'task': 'add_posts.tasks.check_not_available_accounts',
+    #     'schedule': crontab(
+    #         minute='*/50')
+    # },
     'start_parsing_url_new': {
         'task': 'add_posts.tasks.start_parsing_url_new',
         'schedule': crontab(
